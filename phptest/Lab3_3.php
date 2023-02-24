4f84a5491fd7ea7bb0277646417be56b1092fb20
NhatMi <103920552+NhatMi@users.noreply.github.com>
2023-02-24 08:50:08 +0700

    Add files via upload

---
+++ phptest/Lab3_3.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai 3</title>
+</head>
+<style>
+    .display-box {
+        font-family: 'Orbitron', sans-serif;
+        background-color: #dcdbe1;
+        color: black;
+        width: 380px;
+        height: 100px;
+        border: 2px solid black;
+        border-collapse: collapse;
+    }
+    table{
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+
+    }
+    h1{
+        text-align: center;
+    }
+    input{
+        width: auto;
+    }
+    .box_3{
+        font-family: 'Orbitron', sans-serif;
+        background: #3b759e;
+        color: black;
+        border: 2px solid rgb(17, 213, 27);
+        border-collapse: collapse;
+        width: 720px;
+        height: 250px;
+    }
+</style>
+<body>
+<tr>
+        <h1>TÌM HIỂU KỸ THUẬT DOM TRONG JAVASCRIPT <br>
+            (DOM : Document Object Model)</h1>
+    </tr>
+    <table class="display-box" border="1">
+    <tr>
+        <td>Giá trị</td>
+        <td> <input type="text"></td>
+    </tr>
+    <td colspan="2">
+            <input type="button" value="Thêm">
+            <input type="button" value="Thêm ngẫu nhiên">
+        </td>
+    </table>
