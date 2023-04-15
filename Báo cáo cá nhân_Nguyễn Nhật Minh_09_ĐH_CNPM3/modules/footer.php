7faf4f963ce32db274ea9ee70bd339c11f715893
NhatMi <103920552+NhatMi@users.noreply.github.com>
2023-04-16 00:32:38 +0700

    Add files via upload

---
+++ Báo cáo cá nhân_Nguyễn Nhật Minh_09_ĐH_CNPM3/modules/footer.php
@@ -0,0 +1,70 @@
+<footer id="footer">
+		<div class="container">
+			<div class="f1">
+				<div class="col-xs-12 col-sm-3">
+					<div class="tit-boxmain">
+						<ul>
+							<label>THÔNG TIN CÔNG TY</label>
+							<li><a href="">Giới thiệu</a></li>
+							<li><a href="">2018 - Bản quyền của Công Ty TNHH SX & TM Góc Tiên</a></li>
+							<li><a href="">236B Lê Văn Sỹ, Phường 1, Quận Tân Bình, TP Hồ Chí Minh</a></li>
+							<li><a href="">Mã bưu chính 700000</a></li>
+							<li><a href="">Điện thoại: 0389 521 580</a></li>
+						</ul>
+					</div>				
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<div class="tit-boxmain">
+						<!-- <h3><span>THÔNG TIN CÔNG TY</span></h3> -->
+					</div>
+					<div class="ct-boxft1">
+						<ul>
+						<ul>
+							<li><a href="">Giới thiệu về Fairy Corner</a></li>
+							<li><a href="">Chính sách & Quy định chung</a></li>
+							<li><a href="">Quy định và hình thức thanh toán</a></li>
+							<li><a href="">Chính sách bảo hành</a></li>
+							<li><a href="">Chính sách vận chuyển và giao nhận</a></li>
+							<li><a href="">Chính sách bảo mật thông tin cá nhân</a></li>
+							<li><a href="">Chính sách đổi/ trả hàng và hoàn tiền</a></li>
+						</ul>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<div class="tit-boxmain">
+						<h3><span></span></h3>
+					</div>
+					<div class="ct-boxft1">
+						<ul>
+							<li><a href="">Các Câu Hỏi Thường Gặp</a></li>
+							<li><a href="">Hợp Tác Kinh Doanh</a></li>
+							<li><a href="">Tuyển Dụng</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3">
+					<div class="tit-boxmain">						
+					</div>
+					<div class="ct-boxft1">
+						<ul>							
+						<label>Hotline đặt hàng 0389521580 (Từ 09h00 - 18h00, tất cả các ngày)</label>
+						<label>Góp ý - khiếu nại: 0389521580</label>
+						<label>KẾT NỐI VỚI FAIRY TẠI</label>
+							<li><a href="">Facebook</a></li>
+							<li><a href="">Instagram</a></li>
+							<li><a href="">Youtube</a></li>
+						</ul>
+					</div>
+		</div>
+	</footer>
+	<!-- <div class="absolute-footer dark medium-text-center text-center">
+  			<div class="container clearfix">
+   				 <div class="footer-primary pull-left">
+           			 <div class="copyright-footer">
+       					" Copyright 2023 © " 
+						 <strong>Fairy Corner</strong>      
+					</div>
+         	 	</div>
+  			</div>
+		</div> -->
